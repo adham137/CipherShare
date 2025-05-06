aa8b7c83c58bdbfd81e2a8d1a7de87cfd4a9807a
Adham Kassab <adhams.ka7@gmail.com>
2025-05-06 19:04:51 +0300

    New access control commands

--- src/utils/commands_enum.py
+++ src/utils/commands_enum.py
@@ -6,19 +6,24 @@ class Commands(Enum):
     REGISTER_USER = auto()
     LOGIN_USER = auto()
     VERIFY_SESSION = auto()
-    REGISTER_PEER = auto()  # Maybe removed (currently malhash lazma)
+    REGISTER_PEER = auto()
     GET_PEERS = auto()
     REGISTER_FILE = auto()
     GET_FILES = auto()
     REQUEST_KEY = auto()
+    
+    # commands for access control
+    SHARE_FILE = auto()
+    REVOKE_ACCESS = auto()
+    CHECK_ACCESS = auto() 
 
-    # Client-to-Peer Commands
+    # client-to-Peer Commands
     UPLOAD = auto()
     DOWNLOAD = auto()
 
     # Control Signals
-    DONE = auto() 
-    ERROR = auto() 
+    DONE = auto()
+    ERROR = auto()
 
     # string -> enum
     @classmethod
@@ -26,8 +31,8 @@ class Commands(Enum):
         try:
             return cls[command_str.upper()]
         except KeyError:
-            return None 
+            return None
 
-    
     def __str__(self):
         return self.name
+
